b8ff162010aa88f10028c1cf0b81d2aa984dc988
SALMA RIAZ <srabbasiabbasi@gmail.com>
2025-09-08 02:42:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  // Join a room
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+  });
+  socket.on("leave", (roomId) => {
+    socket.leave(roomId);
+  });
+
+  //   Broadcast to room
+  socket.on("send", (message) => {
+    console.log(message);
+    socket.to(message.room).emit("message", message);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Realtime Socket Chat Server</h1>");
+});
+server.listen(5050, () => {
+  console.log("listening on *:5050");
+});
